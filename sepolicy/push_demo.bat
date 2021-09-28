0913e8362361b6587ac884eacf2e70ead3335fc0
Xie Bo <xieb@superred.com.cn>
2021-09-28 11:50:21 +0800

    策略文件从 third_party_selinux 中分离出来
    Signed-off-by: Xie Bo <xieb@superred.com.cn>

---
+++ sepolicy/push_demo.bat
@@ -0,0 +1,3 @@
+hdc_std.exe file send demoloop /bin/
+hdc_std.exe shell chmod 777 /bin/demoloop
+pause
---
+++ sepolicy/push_demo_abcd.bat
@@ -0,0 +1,5 @@
+hdc_std.exe file send rmabcd.sh /bin/
+hdc_std.exe shell chmod 777 /bin/rmabcd.sh
+hdc_std.exe shell /bin/rmabcd.sh
+
+pause
