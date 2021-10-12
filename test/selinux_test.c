82fd934846638857a32ce660295f45fbfa15b524
Qin Fandong <qinfd@superred.com.cn>
2021-10-12 09:58:29 +0800

    LICENSE 放到顶级目录以避免 gitee 的警告
    Signed-off-by: Qin Fandong <qinfd@superred.com.cn>

--- test/selinux_test.c
+++ test/selinux_test.c
@@ -1,3 +1,18 @@
+/* Copyright 2021 北京万里红科技有限公司
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include <ctype.h>
 #include <fcntl.h>
 #include <getopt.h>
@@ -15,13 +30,15 @@ extern int setexeccon(const char *con);
 
 int main(int argc, char *argv[]) {
   FILE *fp = NULL;
+  char buf[1000];
 
   int ret = setcon("u:r:kernel:s0");
   printf("setcon %d\n", ret);
   ret = setexeccon("u:r:kernel:s0");
   printf("setexeccon %d\n", ret);
-  char buf[1000];
+
   sleep(5);
+
   while (1) {
     fp = fopen("/data/abcd.txt", "r");
     if (fp != NULL) {
